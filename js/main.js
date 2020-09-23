7932bbec5884f571dd4555a726a1288f5ad2e491
Diana Sicevaia <sicevaiadiana@gmail.com>
2020-09-23 13:44:23 +0300

    fixing mistaces 2

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ let getLocalDate = (date, isSeconds = false, isISO = false) => {
     : date.toLocaleString().replace(reg, '');
   else {
     const year = date.getFullYear();
-    const month = date.getMonth() + 1 < 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+    const month = date.getMonth() + 1 <= 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
     const day = date.getDate() < 9 ? `0${date.getDate()}` : date.getDate();
     const hour = date.getHours() < 9 ? `0${date.getHours()}` : date.getHours();
     const minutes = date.getMinutes() < 9 ? `0${date.getMinutes()}` : date.getMinutes();
